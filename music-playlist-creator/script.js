eaf3896fc566cf9ac57fc627f2dc3635d9d74515
Leia Marie Spagnola <leia@meta.com>
2025-06-04 09:57:33 -0700

    Modal and tinted overlay created; basic javascript to show/hide modal implemented

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,19 @@
+// get reference to overlay div, which contains screen tint and modal itself
+const overlay = document.getElementById("overlay");
 
+// makes overlay and modal visible
+const showModal = () => {
+    overlay.style.display = "block";
+}
+
+// makes overlay and modal invisible
+const hideModal = () => {
+    overlay.style.display = "none";
+}
+
+// hides modal when it is visible and any area outside it is clicked
+window.onclick = function(event) {
+    if (event.target == overlay) {
+        hideModal();
+    }
+}
