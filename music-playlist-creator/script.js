0e502339fcd46cf1d9f247907e600e11a8dd2e11
Leia Marie Spagnola <leia@meta.com>
2025-06-04 11:46:14 -0700

    clean up modal appearance; add animation on modal display

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,14 +1,22 @@
 // get reference to overlay div, which contains screen tint and modal itself
 const overlay = document.getElementById("overlay");
+// get reference to modal
+const modal = document.getElementById("modal");
 
-// makes overlay and modal visible
-const showModal = () => {
-    overlay.style.display = "block";
+// makes overlay and modal visible, disables scrolling
+const showModal = (playlistData) => {
+    // place playlist information into modal here
+
+    overlay.style.opacity = "1";
+    document.body.style.overflow = "hidden";
+    overlay.style.zIndex = "2";
 }
 
-// makes overlay and modal invisible
+// makes overlay and modal invisible, enables scrolling
 const hideModal = () => {
-    overlay.style.display = "none";
+    overlay.style.opacity = "0";
+    document.body.style.overflow = "scroll";
+    overlay.style.zIndex = "-1";
 }
 
 // hides modal when it is visible and any area outside it is clicked
